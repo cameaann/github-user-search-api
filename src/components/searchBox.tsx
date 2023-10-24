19d01d40751173cfc2ddb85c3796099614b4d0f0
Anna <cameaann@gmail.com>
2023-10-25 00:26:26 +0300

    edited search

--- src/components/searchBox.tsx
+++ src/components/searchBox.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Icon from "./Icon";
 
 function SearchBox() {
   return (
-    <div className="search-box">
+    <div className="search-container">
+      <div className="search-box">
+        <Icon
+          className="item-icon icon-search"
+          icon="search"
+          size={16}
+          color="#4b6a9b"
+        ></Icon>
+        <input className="input-search" type="text" placeholder="Search Github username" />
+      </div>
 
+      <button>Search</button>
     </div>
   );
 }
