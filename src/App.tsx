971573db935a7770606869527d43a2c2790ee226
Anna <cameaann@gmail.com>
2023-10-25 00:11:23 +0300

    added all icons and edited app-header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ProfileInfo from "./components/profileInfo";
 import SearchBox from "./components/searchBox";
+import Icon from "./components/Icon";
 
 function App() {
   return (
     <div className="container-wrap">
       <main>
         <header className="app-header">
-          <h2>Devfinder</h2>
-          <button className="btn switch-mode">Dark</button>
+          <h2 className="app-header-title">devfinder</h2>
+          <button className="btn">
+          <span className="btn-word">Dark</span>
+          <Icon className="item-icon icon-moon" icon="moon" size={16} color="#4b6a9b"></Icon>
+          </button>
         </header>
         <SearchBox/>
         <ProfileInfo/>
